171e20e95324104cc6974069ca2f08ca30df977b
Maxim <maxiimruso7@gmail.com>
2020-11-28 22:51:34 +0100

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb+srv://maxim_n_dev:jHcIFCWdzJ0WgPw8@cluster0.dyb5e.mongodb.net/cafe';
+    urlDB = process.env.DB_URI;
 }
 
 process.env.URL_DB = urlDB;
